bc5fa9d3e4ce6f96b9fecad405f522e93db606a6
Citrons06 <yeolee1212@gmail.com>
2023-08-05 21:52:03 +0900

    study0805

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,38 +1,34 @@
 package hello.core.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-	
+
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 	
 	@Autowired
-	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+	public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
+		this.discountPolicy = discountPolicy; 
 	}
-	
-	/*
-	 * @Autowired public void init(MemberRepository memberRepository, DiscountPolicy
-	 * discountPolicy) { this.memberRepository = memberRepository;
-	 * this.discountPolicy = discountPolicy; }
-	 */
 	
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
-		Member member = memberRepository.findById(memberId);			// 회원 정보 조회
-		int discountPrice = discountPolicy.discount(member, itemPrice);	// 할인 정책에 회원 정보 넘김
-		
-		return new Order(memberId, itemName, itemPrice, discountPrice);	// Order 객체 생성
+		Member member = memberRepository.findById(memberId); // 회원 정보 조회
+		int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책에 회원 정보 넘김
+
+		return new Order(memberId, itemName, itemPrice, discountPrice); // Order 객체 생성
 	}
-	
+
 	// 테스트 용도
 	public MemberRepository getMemberRepository() {
 		return memberRepository;

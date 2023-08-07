5a141e8056c1b0efb6097c564f154286c8ead57c
Citrons06 <yeolee1212@gmail.com>
2023-08-07 23:17:33 +0900

    study0807

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,18 +15,18 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 	
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;				// 의존관계 주입이 일어날 때 myLogger를 내놓아야 하는데 request가 없음
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 	
 	@RequestMapping("log-demo")
 	@ResponseBody	// 화면 없이 문자로 바로 반환
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestURL(requestURL);			// 로그에 url 남김
 		
 		myLogger.log("controller test");
 		logDemoService.logic("testId");
 		return "OK";
 	}
-	
 
 }
